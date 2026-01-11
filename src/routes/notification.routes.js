0cb7d6b921dc0ebfc88ec744c628aabeafbf2ea8
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2026-01-11 21:24:14 +0530

    Remove backend code (migrated to separate repo)

--- src/routes/notification.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notification.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.get('/', authMiddleware, notificationController.getNotifications);
-router.patch('/:notificationId/read', authMiddleware, notificationController.markAsRead);
-
-module.exports = router;
